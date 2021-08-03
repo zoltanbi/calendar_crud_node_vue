42b9447a56917318d920e7ddc4b54a7bd40dcc68
Zoltanbi <zoltanbiro001@gmail.com>
2021-08-03 09:58:27 -0700

    Configured Hapi server route

---
+++ server/routes/index.js
@@ -0,0 +1,12 @@
+"use strict"
+
+module.exports.register = async server => {
+    server.route({
+        method: "GET",
+        path: "/",
+        handler: async (request, h) => {
+            return "My first hapi server"
+        }
+    });
+};
+
